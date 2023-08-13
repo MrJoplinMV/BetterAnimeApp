0b7da676776d69976a72be7567bc8f6a7c6b57c0
MrJoplinMV <gamererik725@gmail.com>
2023-08-13 18:16:19 -0300

    commit

--- resources/app/preload.js
+++ resources/app/preload.js
@@ -1,18 +1,11 @@
 const { ipcRenderer } = require('electron');
 
-// Verifica se o site está ativo
 function isSiteAtivo() {
-  // Aqui podemos adicionar a lógica para verificar se o site está ativo.
-  // Fazendo uma request http ou verificando a presença de um elemento específico.
-
-  // Retorne true se estiver ativo e false caso contrário.
-  return true; // Exemplo considerando o site sempre ativo.
+  return true;
 }
 
-// Enviar o resultado para o processo principal
 ipcRenderer.send('SiteAtivo', isSiteAtivo());
 
-// Expondo as funções para a janela de renderização
 window.defineInSession = function () {
   ipcRenderer.send('defineInSession');
 };
